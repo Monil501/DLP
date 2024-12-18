80f34be2c0b3f661a430eb232cb8200bd8516d56
Monil501 <141821257+Monil501@users.noreply.github.com>
2024-12-18 15:43:23 +0530

    Update PR-1.c

--- PR-1.c
+++ PR-1.c
@@ -1,3 +1,4 @@
+//For regular expression a*bb
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -45,5 +46,52 @@ int main() {
             printf("String: %s -> Invalid\n", str);
         }
     }
+    return 0;
+}
+
+
+//for regular expression a+bb
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool validateString(const char* str) {
+    int len = strlen(str);
+    int i = 0;
+    
+    if (str[i] == 'a') {
+        while (i < len && str[i] == 'a') {
+            i++; 
+        }
+    } else {
+        return false; 
+    }
+
+    if (str[i] == 'b' && str[i + 1] == 'b') {
+        i += 2; 
+        return i == len; 
+    }
+
+    return false;
+}
+
+int main() {
+ 
+    const char* strings[] = {
+        "aabb", "abb", "bb", "aaaabb", "ab", "aaabb", "bbb", "aabbb",
+        "aaaaaaabb",NULL
+    };
+
+    printf("String Validation Results:\n");
+    for (int i = 0; strings[i] != NULL; i++) {
+        const char* str = strings[i];
+
+        if (validateString(str)) {
+            printf("String: %s -> Valid\n", str);
+        } else {
+            printf("String: %s -> Invalid\n", str);
+        }
+    }
+
     return 0;
 }
